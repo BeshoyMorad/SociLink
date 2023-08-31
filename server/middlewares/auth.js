acf981ad0f3ba349d02d7f1858ec3e12728d0e78
BeshoyMorad <besh0morta@gmail.com>
2023-08-31 14:49:44 +0300

    Fix verifyToken middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
-const verifyToken = (req, res, next) => {
+export const verifyToken = (req, res, next) => {
   try {
     let token = req.header("Authorization");
     if (!token) return res.status(401).json({ error: "Access denied" });
 
-    if (token.startWith("Bearer "))
+    if (token.startsWith("Bearer "))
       token = token.slice(7, token.length).trimLeft();
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
