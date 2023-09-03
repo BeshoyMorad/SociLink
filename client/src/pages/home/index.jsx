8a4e76b4fb79385e1c9a106b1db95019435fff4d
BeshoyMorad <besh0morta@gmail.com>
2023-09-03 20:50:37 +0300

    Prepare home page and add user widget component

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,11 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Grid, useMediaQuery } from "@mui/material";
 import Navbar from "pages/navbar";
-import React from "react";
+import UserWidget from "pages/widgets/UserWidget";
+import { useSelector } from "react-redux";
 
 function HomePage() {
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
   return (
     <Box>
       <Navbar />
+
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          p: "2rem 3%",
+        }}
+      >
+        <Grid item xs={isNonMobileScreens ? 3 : 12}>
+          <UserWidget userId={user._id} />
+        </Grid>
+
+        <Grid item xs={isNonMobileScreens ? 6 : 12}>
+          {/* Friends list */}
+        </Grid>
+
+        {isNonMobileScreens && (
+          <Grid item xs={3}>
+            {/* Friends list */}
+          </Grid>
+        )}
+      </Grid>
     </Box>
   );
 }
